9652b768588f2ffcce888c72341e19d1997be1a7
Vinit Patel <vinitpatel476@gmail.com>
2023-12-07 03:12:28 -0500

    starting over

--- controllers/autRoutes.js
+++ controllers/autRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const session = require("express-session");
-const {User,Likes, Posts} = require('../models');
+const { User, Likes, Posts } = require('../models');
 // require("dotenv").config();
 // const exphbs = require("express-handlebars");
 // const sequelize = require("./config/connection");
@@ -14,49 +14,49 @@ const {User,Likes, Posts} = require('../models');
 // const SequelizeStore = require("connect-session-sequelize")(session.Store);
 // Registration route
 router.post("/register", (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  bcrypt.hash(password, saltRounds, (err, hash) => {});
+    bcrypt.hash(password, saltRounds, (err, hash) => {});
 });
 
 //session data
 
-router.get("/sessiondata",(req,res)=>{
-  res.json(req.session)
-})  
+router.get("/sessiondata", (req, res) => {
+    res.json(req.session)
+})
 
 
-router.get("/signup",(req,res)=>{
+router.get("/signup", (req, res) => {
     res.render("signup")
 });
 
-router.get("/login",(req,res)=>{
+router.get("/login", (req, res) => {
     res.render("login");
 });
 
-router.get("/logout",(req,res)=>{
+router.get("/logout", (req, res) => {
     res.render("logout");
 });
 
 // Login route
 
-router.post("/login",(req,res)=>{
+router.post("/login", (req, res) => {
     //1. find the user who is trying to login
-    res.render("login");
+    // res.render("login");
     User.findOne({
-        where:{
-            username:req.body.username
+        where: {
+            username: req.body.username
         }
-    }).then(foundUser=>{
-        if(!foundUser){
-            res.status(401).json({msg:"Invalid username/password"})
+    }).then(foundUser => {
+        if (!foundUser) {
+            res.status(401).json({ msg: "Invalid username/password" })
         } else {
-            if(!bcrypt.compareSync(req.body.password,foundUser.password)){
-                res.status(401).json({msg:"Invalid username/password"})
+            if (!bcrypt.compareSync(req.body.password, foundUser.password)) {
+                res.status(401).json({ msg: "Invalid username/password" })
             } else {
                 req.session.user = {
-                    id:foundUser.id,
-                    username:foundUser.username
+                    id: foundUser.id,
+                    username: foundUser.username
                 }
                 res.json(foundUser)
             }
@@ -64,12 +64,12 @@ router.post("/login",(req,res)=>{
     })
 })
 
-router.get("/logout",(req,res)=>{
-  req.session.destroy();
-  res.send("logged out!")
-})
-// app.post("/login", (req, res) => {
-//   const { username, password } = req.body;
+router.get("/logout", (req, res) => {
+        req.session.destroy();
+        res.send("logged out!")
+    })
+    // app.post("/login", (req, res) => {
+    //   const { username, password } = req.body;
 
 //   bcrypt.compare(password, hashedPassword, (err, result) => {
 //     if (result) {
